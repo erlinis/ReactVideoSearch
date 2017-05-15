71669bf747e021515df0aa88151019f93314bebb
Erlinis Quintana <erlinis.quintana@gmail.com>
2017-05-15 02:41:54 -0500

    transforming App in a class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,15 +10,13 @@ class SearchBar extends Component {
   render() {
     return(
       <div>
-        <input onChange={event => { this.setState({ term: event.target.value}) }} />
+        <input
+          value={this.state.term}
+          onChange={event => { this.setState({ term: event.target.value}) }} />
       </div>
     );
   }
 
-  // onInputChange(event) {
-  //   // console.log(event.target.value);
-  //   this.setState({ term: event.target.value });
-  // }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY_YOUTUBE = 'AIzaSyCaC7rwgTS6T9ZHm2JbNLp-mZtOSN6APRc';
 
-// Create a new component.
-// This component should produce some HTML
-const App = function(){
- return(
-   <div>
-     <SearchBar />
-   </div>
- );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+  this.state = { videos: [] };
+
+  YTSearch({key: API_KEY_YOUTUBE, term: 'shakira'}, (videos) => {
+    this.setState({ videos });
+    console.warn(videos);
+  });
+
+  }
+  render(){
+     return(
+       <div>
+         <SearchBar />
+       </div>
+     );
+  }
 }
 
 
