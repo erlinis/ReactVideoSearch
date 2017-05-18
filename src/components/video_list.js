23a378599af65c21f2cff92466f22153a253cea8
Erlinis Quintana <erlinis.quintana@gmail.com>
2017-05-18 00:14:17 -0500

    Adding the video item body

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import VideoItem from './video_item';
 
 const VideoList = (props) => {
+
+ const videoItems = props.videos.map((video) => {
+   return <VideoItem key={video.etag} video={video} />
+ });
+
  return (
    <ul className="col-nd-4 list-group">
-     {props.videos.length}
+     {videoItems}
    </ul>
   );
 }
